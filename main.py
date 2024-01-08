22bc96f1fdcab4db92876bbe2b7fa28e15c1447a
C. Vanessa Del Rio Ortiz <unrqe@student.kit.edu>
2024-01-08 23:55:49 +0100

    FIRST COMMIT - uploading sparrow gan

---
+++ main.py
@@ -0,0 +1,160 @@
+import os
+import torch 
+from models.gan import Generator as G, Discriminator as D
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+import hydra 
+import utils.Trainer as uTrainer
+from omegaconf import DictConfig, OmegaConf
+from torchvision.utils import make_grid
+from torch.utils.tensorboard import SummaryWriter as OutputTo
+
+# Get current working directory - work only in 
+# the current top directory!! ../rl-autoencoder/
+path = os.getcwd()
+path_to_cwd = ""
+
+manualSeed = 814
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+torch.use_deterministic_algorithms(True) 
+# check if cuda available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f'Selected device: {device}')
+
+# custom weights initialization called on ``netG`` and ``netD``
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0)
+
+# hydra config
+@hydra.main(config_path='conf', config_name='config')
+def __main__(cfg: DictConfig)->None:
+    run = 2
+    # Define directories
+    outimgs =path + path_to_cwd+str(cfg.data.out_imgs)
+    outtb = path + path_to_cwd + str(cfg.data.out_tb)
+    ds_fd= path + path_to_cwd + str(cfg.datasets.folder)
+    try:
+        os.makedirs(outimgs)
+        os.makedirs(outtb)
+    except FileExistsError:
+        pass
+    # Creating batch of images
+    train_ds = uTrainer.SparrowDataset(ds_fd )
+    train_loader = uTrainer.custom_loader(ds_fd,              
+                                                    cfg.model.image.size,cfg.model.image.size,
+                                                    cfg.model.image.color,cfg.model.image.batch)
+    print("Train Loader finished")
+
+    # Configuring SummaryWriter
+    writer = OutputTo(outtb,comment="First try")
+    
+    
+    ######
+    # Create the generator
+    netG = G(3).to(device)
+    if torch.cuda.is_available():
+        netG.cuda()
+
+    # Apply the ``weights_init`` function to randomly initialize all weights
+    #  to ``mean=0``, ``stdev=0.02``.
+    netG.apply(weights_init)
+
+    # Create the Discriminator
+    netD = D(3).to(device)
+
+    if torch.cuda.is_available():
+        netD.cuda()
+
+    # Apply the ``weights_init`` function to randomly initialize all weights
+    # like this: ``to mean=0, stdev=0.2``.
+    netD.apply(weights_init)
+
+    #######   
+    # define loss function
+    # ist MSE es gut für Bilder?
+    loss_function = torch.nn.BCELoss()
+    #Noise
+    # Create batch of latent vectors that we will use to visualize
+    #  the progression of the generator
+    fixed_noise = torch.randn(64, 100, 1, 1, device=device) # 50 size og gen input
+    
+    # Using an Adam Optimizer
+    optimizerD = torch.optim.Adam(netD.parameters(),
+                                lr = cfg.training.lr,
+                                betas=(cfg.training.beta1, 0.999))
+    optimizerG = torch.optim.Adam(netG.parameters(),
+                                lr = cfg.training.lr,
+                                betas=(cfg.training.beta1, 0.999))
+    
+    epochs = cfg.training.epochs
+    trainR_loss = []
+    trainF_loss = []
+    img_list = []
+    #initialize trainer
+    trainer = uTrainer.Trainer(netD,netG,device,optimizerD,optimizerG,loss_function)
+    for epoch in range(epochs):
+        ##############
+        ### TRAIN  ###
+        ##############
+        lossR,lossF,D_x,D_G_z1,D_G_z2= trainer.train(train_loader=train_loader)
+        trainR_loss.append(lossR)
+        trainF_loss.append(lossF)
+        # Adding train loss to tensorboard per epoch
+        writer.add_scalar('LossReal/train', np.mean(trainR_loss), epoch)
+        writer.add_scalar('LossFake/train', np.mean(trainF_loss), epoch)
+        writer.add_scalar('D_x/train', D_x, epoch)
+        writer.add_scalar('D_G_z1/train', D_G_z1, epoch)  
+        writer.add_scalar('D_G_z2/train', D_G_z2, epoch)
+        #print(train_loss)
+      
+        ################
+        ###  VALIDATE ###
+        #################
+        #val_loss=trainer.validate(val_loader=val_loader)
+        # Adding validation loss to tensorboard per epoch
+        #writer.add_scalar('Loss/validation', val_loss, epoch)
+
+        #@DEPRECATED:
+        print('\n EPOCH {}/{} \t real train loss {} \t fake train loss {} '.
+              format(epoch + 1, epochs,np.mean(trainR_loss),np.mean(trainF_loss)))
+        # Get model
+        model = trainer.get_model()
+        ##################
+        ## VISUALIZATION #
+        ##################
+        ##https://medium.com/dataseries/convolutional-autoencoder-in-pytorch-on-mnist-dataset-d65145c132ac
+        # Part 4
+        if epoch % 15 == 0:
+            plt.figure(figsize=(10,10)),
+            classes=1
+            #targets = test_ds.targets.numpy()
+            #t_idx = {i:np.where(targets==i)[0][0] for i in range(classes)}
+            for i in range(classes):
+                model.eval()
+                with torch.no_grad():
+                    reconstructed = model(fixed_noise).detach().cpu()
+                img_list.append(make_grid(reconstructed,padding=2, normalize=True))
+            # Plot the fake images from the last epoch
+            plt.axis("off")
+            plt.title("Fake Images")
+            plt.imshow(np.transpose(img_list[-1],(1,2,0)))      
+            plt.savefig(outimgs + "rec"+str(epoch) )
+            plt.close()
+    writer.flush()
+    state = {
+        'epoch': epoch,
+        'stateG_dict': model.state_dict(),
+        'stateD_dict':trainer.get_D.state_dict(),
+        'optimizerD': trainer.get_optD.state_dict(),
+        'optimizerG':trainer.get_optG.state_dict(),
+    }
+    torch.save(state, outimgs +"state.pyc")
+if __name__ == '__main__':
+    __main__()
---
+++ utils/Trainer.py
@@ -0,0 +1,194 @@
+import torch
+from numpy import mean 
+from torch.utils.data import random_split
+from torchvision import datasets as ds 
+from torchvision import transforms as tfs
+import glob
+from numpy import array
+from PIL import Image
+class Trainer():
+    """
+    Defines how training of Autoenconder is taking place
+    """
+    def __init__(self,modelD,modelG, device, optD,optG, lf):
+        """_Initializes trainer_
+
+        Args:
+            model (_torch.nn.Module_): _description_
+            device (_torch.device_): _cpu or cuda_
+            opt (_torch.optim_): _choosen optimizer function_
+            lf (_torch.nn_): _choosen  loss function_
+        """
+        self.dev = device
+        self.mod = modelG
+        self.disc= modelD
+        self.optD = optD
+        self.optG = optG
+        self.lf = lf
+        self.rl= 1.
+        self.fl= 0.
+       
+      
+    def train(self, train_loader):
+        """_Train the model for n elements in the train loader_
+
+        Args:
+            train_loader : _Train loader object with elements for the model to train_
+
+        Returns:
+            _double_: _Returns mean loss value of the batch_
+        """
+        
+        trainR_loss=[]
+        trainF_loss =[]
+        D_x=0
+        D_G_z1=0
+        D_G_z2=0
+        for (image, i) in train_loader:
+            
+            self.disc.zero_grad()
+            # Train real
+            real_cpu = image.to(self.dev)
+            b_size = real_cpu.size(0)
+            label = torch.full((b_size,), self.rl, dtype=torch.float, device=self.dev)
+        
+            output = self.disc(real_cpu).view(-1)
+            lossR=self.lf(output,label)
+            lossR.backward()
+            D_x = output.mean().item()
+            trainR_loss.append(lossR.detach().cpu().numpy())
+            ## Train with all-fake batch
+            # Generate batch of latent vectors
+            noise = torch.randn(b_size, 100, 1, 1, device=self.dev)
+            # Generate fake image batch with G
+            fake = self.mod(noise)
+            label.fill_(self.fl)
+            # Classify all fake batch with D
+            output = self.disc(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            lossF = self.lf(output, label)
+            lossF.backward()
+            trainF_loss.append(lossF.detach().cpu().numpy())
+
+            loss = lossR + lossF
+            D_G_z1 = output.mean().item()
+            #Update
+            self.optD.step()
+            ### Train G  
+            self.mod.zero_grad() 
+            label.fill_(self.rl)
+            output = self.disc(fake).view(-1)
+            lossG = self.lf(output,label)
+            lossG.backward()
+            D_G_z2 = output.mean().item()
+            self.optG.step()
+        return mean(trainR_loss),mean(trainF_loss), D_x, D_G_z1,D_G_z2
+    
+
+    def get_model(self):
+        """_Gets current model trained/evaluated_
+
+        Returns:
+            _torch.nn.Module_: _trained model_
+        """
+        return self.mod
+    def get_D(self):
+        return self.disc
+    
+    def get_optD(self):
+        return self.optD
+    
+    def get_optG(self):
+        return self.optG
+
+def MNIST_loader(ds_folder,batch_size=256, color=False):
+    train_dataset = ds.MNIST(ds_folder,train=True,download=True)
+    test_dataset = ds.MNIST(ds_folder,train=False,download=True)
+    # transform to grayscale
+    train_tf = tfs.Compose([tfs.ToTensor(),tfs.Grayscale(),])
+    test_tf=tfs.Compose([tfs.ToTensor(),tfs.Grayscale(),])
+    
+    m=len(train_dataset)
+    
+    train_dataset.transform=train_tf
+    test_dataset.transform = test_tf
+    train_data, val_data = random_split( train_dataset,  [int(m-m*0.2), int(m*0.2)])
+    # loaders
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,shuffle=True)
+    return train_loader, val_loader,test_loader,test_dataset
+
+
+def custom_loader(train_ds, h=64,w=64,color = True, batch = 10):
+   
+    # transform to grayscale
+    if color is False:
+        dataset=ds.ImageFolder(root=train_ds,transform= tfs.Compose([tfs.Resize((h, w)),tfs.ToTensor(),tfs.Grayscale(),
+                                tfs.Normalize((0.5),(0.5)),
+                            ]))
+        
+    else:
+        dataset_og = ds.ImageFolder(root = train_ds,
+                            transform=tfs.Compose([tfs.Resize((h, w)),tfs.ToTensor(),
+                                tfs.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),
+                            ]))
+        # Augment the dataset with mirrored images
+        mirror_dataset = ds.ImageFolder(root = train_ds,
+                            transform=tfs.Compose([tfs.Resize((h, w)),
+                                    tfs.RandomHorizontalFlip(p=1.0),
+                                    tfs.ToTensor(),
+                                tfs.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),
+                            ]))
+        sharp_dataset = ds.ImageFolder(root = train_ds,
+                            transform=tfs.Compose([tfs.Resize((h, w)),
+                                tfs.RandomAdjustSharpness(sharpness_factor=2),
+                                tfs.ToTensor(),
+                                tfs.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),
+                            ]))
+        autocontrast_dataset=ds.ImageFolder(root = train_ds,
+                            transform=tfs.Compose([tfs.Resize((h, w)),
+                                tfs.RandomAutocontrast(),
+                                tfs.ToTensor(),
+                                tfs.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),
+                            ]))
+    dataset = torch.utils.data.ConcatDataset([dataset_og,mirror_dataset,sharp_dataset,autocontrast_dataset])     
+    # loaders
+    train_loader = torch.utils.data.DataLoader(dataset,batch_size=batch,
+                                         shuffle=True,num_workers=2)
+    return train_loader
+
+ 
+class SparrowDataset(torch.utils.data.Dataset):
+    """_Maps Dataset for FZI Project_"""
+
+    def __init__(self, root_dir,color = False,transform = None):
+        file_list = glob.glob(root_dir + "/*")
+        self.data = []
+        labels=[]
+        self.targets=[]
+        for class_path in file_list:
+            class_name = class_path.split("/")[-1]
+            label = 0 if class_name.lower()=='train' else 1
+            for img_path in glob.glob(class_path + "/*.jpg"):
+                self.data.append([img_path, class_name])
+            self.class_map = { "train":0, "test":1}
+
+        self.transform = transform
+        #self.samples = samples
+        self.targets=[s[0] for s in self.targets]
+        
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self,idx):
+        img_path, class_name = self.data[idx]
+        img = Image.open(img_path)
+        class_id = self.class_map[class_name]      
+        class_id = torch.tensor([class_id])
+        if self.transform is not None:
+            img_tensor = self.transform(img)
+
+        #if self.targets is not None:
+
+        return img_tensor, class_id
